7232119dafa24cafc555e90ce530e53fe8010670
Caio Escorel <caioheraclio@gmail.com>
2025-11-16 18:10:05 -0300

    Adicionando cross origin da url do frontend para cadastro de  usuário

--- src/main/java/olia/backend/api/controller/UsuarioController.java
+++ src/main/java/olia/backend/api/controller/UsuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
