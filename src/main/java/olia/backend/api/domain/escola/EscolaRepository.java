262ad607457d88c60eea0765ec3c84b31bbc6d1c
Gabriel Moneta <gabrielpmoneta@gmail.com>
2025-11-23 22:06:19 -0300

    alteração: o Spring JPA não entendeu o nome do método findByEmail_acesso, então coloquei @Query manualmente para dar certo

--- src/main/java/olia/backend/api/domain/escola/EscolaRepository.java
+++ src/main/java/olia/backend/api/domain/escola/EscolaRepository.java
@@ -3,9 +3,11 @@ package olia.backend.api.domain.escola;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EscolaRepository extends JpaRepository<Escola, Long> {
     Page<Escola> findAllByAtivoTrue(Pageable paginacao);
 
-    Escola findByEmail_acesso(String emailAcesso);
+    @Query("SELECT e FROM Escola e WHERE e.email_acesso = :emailAcesso")
+    Escola buscarPorEmailDeAcesso(String emailAcesso);
 }
