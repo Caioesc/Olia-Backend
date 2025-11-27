d3d82379061e00a3c710249cdb07c34a7b9103c9
Caio Escorel <caioheraclio@gmail.com>
2025-11-26 22:11:41 -0300

    testando cross origins

--- src/main/java/olia/backend/api/controller/UsuarioController.java
+++ src/main/java/olia/backend/api/controller/UsuarioController.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
